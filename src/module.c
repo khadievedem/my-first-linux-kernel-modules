3bb489dd98d9020bb189b9417cb0055e774a7aae
khadiev <khadiev.edem@gmail.com>
2022-06-25 21:38:43 +0300

    add file_operations struct

--- src/module.c
+++ src/module.c
@@ -1,21 +1,27 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-
+#include <linux/fs.h>
 
 static int my_init(void)
 {
-    return  0;
+	return 0;
 }
-    
+
 static void my_exit(void)
 {
-    return;
+	return;
 }
-    
+
+ssize_t device_file_read(struct file *, char *, size_t, loff_t *);
+
+static struct file_operations simple_driver_fops = {
+	.owner = THIS_MODULE,
+	.read = device_file_read,
+};
+
+
 module_init(my_init);
 module_exit(my_exit);
 
-
 MODULE_LICENSE("LGPL");
-
