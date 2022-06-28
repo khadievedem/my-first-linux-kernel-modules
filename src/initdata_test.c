4f56e8226634309c9c918fbac82be3452e2cee91
khadiev <khadiev.edem@gmail.com>
2022-06-28 22:58:16 +0300

    add module initdata_test

---
+++ src/initdata_test.c
@@ -0,0 +1,28 @@
+/*
+ * initdata_test.c - 
+*/
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+#define MODULE_NAME "initdata_test"
+
+static int initdata_num __initdata = 11;
+
+static int __init initdata_init(void)
+{
+	printk(KERN_INFO "[%s] Here is __initdata data: %d\n", MODULE_NAME,
+	       initdata_num);
+	return 0;
+}
+
+static void __exit initdata_exit(void)
+{
+	printk(KERN_INFO "[] Exit from the module. \n", MODULE_NAME);
+	return;
+}
+
+module_init(initdata_init);
+module_exit(initdata_exit);
+
+MODULE_LICENSE("LGPL");
