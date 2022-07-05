df8764ce48ef06cd8a5d647c613d566d1b775242
khadiev <khadiev.edem@gmail.com>
2022-07-05 16:45:52 +0300

    make format

--- src/my_simple_module.c
+++ src/my_simple_module.c
@@ -23,11 +23,11 @@ static const char g_s_Hello_World_string[] = "Hello world from kernel mode !\n";
 static const ssize_t g_s_Hello_World_size = sizeof g_s_Hello_World_string;
 
 static struct file_operations simple_driver_fops = {
-    owner : THIS_MODULE,
-    read : device_read,
-    write : device_write,
-    open : device_open,
-    release : device_release
+	owner: THIS_MODULE,
+	read: device_read,
+	write: device_write,
+	open: device_open,
+	release: device_release
 };
 /*
  *
@@ -36,30 +36,30 @@ static struct file_operations simple_driver_fops = {
  */
 int register_device(void)
 {
-    int result = 0;
-    printk(KERN_NOTICE "[%s] register_device() is called.\n", DEVICE_NAME);
-    if ((result = register_chrdev(device_file_major_number, DEVICE_NAME,
-                      &simple_driver_fops)) < 0) {
-        printk(KERN_WARNING
-               "[%s] can't register character device with error code %i\n",
-               DEVICE_NAME, result);
-        return result;
-    }
-    device_file_major_number = result;
-    printk(KERN_NOTICE
-           "[%s] registered character device with major number %i \
+	int result = 0;
+	printk(KERN_NOTICE "[%s] register_device() is called.\n", DEVICE_NAME);
+	if ((result = register_chrdev(device_file_major_number, DEVICE_NAME,
+				      &simple_driver_fops)) < 0) {
+		printk(KERN_WARNING
+		       "[%s] can't register character device with error code %i\n",
+		       DEVICE_NAME, result);
+		return result;
+	}
+	device_file_major_number = result;
+	printk(KERN_NOTICE
+	       "[%s] registered character device with major number %i \
                         and minor numbers 0...255\n",
-           DEVICE_NAME, device_file_major_number);
-    return 0;
+	       DEVICE_NAME, device_file_major_number);
+	return 0;
 }
- 
+
 void unregister_device(void)
 {
-    printk(KERN_NOTICE "[%s] unregister_device() is called.\n",
-           DEVICE_NAME);
-    if (device_file_major_number != 0) {
-        unregister_chrdev(device_file_major_number, DEVICE_NAME);
-    }
+	printk(KERN_NOTICE "[%s] unregister_device() is called.\n",
+	       DEVICE_NAME);
+	if (device_file_major_number != 0) {
+		unregister_chrdev(device_file_major_number, DEVICE_NAME);
+	}
 }
 /*
  *
@@ -67,51 +67,54 @@ void unregister_device(void)
  *
  */
 static ssize_t device_read(struct file *file_ptr, char __user *user_buffer,
-                        size_t count, loff_t *position)
+			   size_t count, loff_t *position)
 {
-    printk(KERN_NOTICE
-            "[%s] Device file is read at offset %i, read bytes count = %u",
-            DEVICE_NAME, (int)*position, (unsigned int)count);
-    /* if we try to read data out of our file => we have nothing to read */
-    if (*position >= g_s_Hello_World_size) {
-        return 0;
-    }
-    if (*position + count > g_s_Hello_World_size) {
-        count = g_s_Hello_World_size - *position;
-    }
-    if (copy_to_user(user_buffer, g_s_Hello_World_string + *position,
-             count) != 0) {
-        return -EFAULT;
-    }
-    *position += count;
-    return count;
+	printk(KERN_NOTICE
+	       "[%s] Device file is read at offset %i, read bytes count = %u",
+	       DEVICE_NAME, (int)*position, (unsigned int)count);
+	/* if we try to read data out of our file => we have nothing to read */
+	if (*position >= g_s_Hello_World_size) {
+		return 0;
+	}
+	if (*position + count > g_s_Hello_World_size) {
+		count = g_s_Hello_World_size - *position;
+	}
+	if (copy_to_user(user_buffer, g_s_Hello_World_string + *position,
+			 count) != 0) {
+		return -EFAULT;
+	}
+	*position += count;
+	return count;
 }
- 
+
 static ssize_t device_write(struct file *file_ptr, const char *user_buffer,
-                            size_t count, loff_t *position) {
-    printk(KERN_WARNING "This operation isn't supported");
-    return -EINVAL;
+			    size_t count, loff_t *position)
+{
+	printk(KERN_WARNING "This operation isn't supported");
+	return -EINVAL;
 }
- 
-static int device_open(struct inode *inode, struct file *file) {
-    /* called when a process tries to open the device file */
-    static int counter;
- 
-    if (is_device_open) {
-        return -EBUSY;
-    }
-    is_device_open++;
-    try_module_get(THIS_MODULE); /*
+
+static int device_open(struct inode *inode, struct file *file)
+{
+	/* called when a process tries to open the device file */
+	static int counter;
+
+	if (is_device_open) {
+		return -EBUSY;
+	}
+	is_device_open++;
+	try_module_get(THIS_MODULE); /*
                                   * increments the use count of device
                                   */
-    return 0;
+	return 0;
 }
- 
-static int device_release(struct inode *inode, struct file *file) {
-    /* called when a process closes the device file */
-    is_device_open--;
-    module_put(THIS_MODULE); /* decrements the use count of the module
+
+static int device_release(struct inode *inode, struct file *file)
+{
+	/* called when a process closes the device file */
+	is_device_open--;
+	module_put(THIS_MODULE); /* decrements the use count of the module
                               * to be able to get rid of them in future
                               */
-    return 0;
+	return 0;
 }
